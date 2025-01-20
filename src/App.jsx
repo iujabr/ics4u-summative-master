3d836754be5963af8e63c4d5067ee8f066efd0a2
Irfan Jabbar <irfan.jabbar@student.tdsb.on.ca>
2025-01-20 00:01:58 -0500

    fourth commit-integrated new files and new folders, as well as updated main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
+//import { StoreProvider } from "path-to-store-provider";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { StoreProvider } from "./context";
+import { StoreProvider } from "../src/context/index";
 import HomeView from "../src/views/HomeView";
-import './App.css'
+import RegisterView from "../src/views/RegisterView";
+import LoginView from "../src/views/LoginView";
+import MoviesView from "../src/views/MoviesView";
+import AllMoviesView from "../src/views/AllMoviesView";
+import DetailMovieView from "../src/views/DetailMovieView";
+import CartView from "../src/views/CartView";
+import ErrorView from "./views/ErrorView";
+import ProtectedRoutes from "./util/ProtectedRoutes";
+//import './App.css'
 
-const App = () => {
+function App() {
 
   return (
     <StoreProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomeView />} />
+          <Route path="/register" element={<RegisterView />} />
+          <Route path="/login" element={<LoginView />} />
+          <Route element={<ProtectedRoutes />}>
+            <Route path="/cart" element={<CartView />} />
+            <Route path="/movies" element={<MoviesView />}>
+              <Route path="all" element={<AllMoviesView />} />
+              <Route path=":id" element={<DetailMovieView />} />
+            </Route>
+          </Route>
+          <Route path="*" element={<ErrorView />} />
         </Routes>
       </BrowserRouter>
     </StoreProvider>
--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,8 +1,21 @@
+import { Link } from "react-router-dom";
 import "./HomeView.css";
 
 const HomeView = () => {
   return (
-    <h1>Home</h1>
+    <div className="hero">
+      <div className="overlay"></div>
+      <header>
+        <div className="buttons">
+          <Link to={`/register`} className="button">Register</Link>
+          <Link to={`/login`} className="button">Login</Link>
+        </div>
+      </header>
+      <div className="hero-content">
+        <h1>Unlimited movies, TV shows, and more.</h1>
+        <p>Watch anywhere. Cancel anytime.</p>
+      </div>
+    </div>
   )
 }
 
